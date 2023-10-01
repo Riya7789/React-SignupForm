2260c46c44cfbd1a67fec2ec77b2076dda6fbdff
Riya7789 <cresthariya7@gmail .com>
2023-10-01 17:38:30 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,34 +1,9 @@
-import fbImg from './facebook.png'
-import twitterImg from './twitter.png'
-import googleImg from './google.png'
 import './App.css';
-import Header from './Header';
-import Button from './Button';
-import myForm from './Form';       
+import SocialSignupForm from './SocialSignupForm'; 
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      <div className="Button">
-      <Button
-        imageSrc={fbImg}
-        name="Facebook"
-        color="btn facebook"
-      />
-      <Button
-        imageSrc={twitterImg}
-        name="Twitter"
-        color="btn twitter"
-      />
-      <Button
-        imageSrc={googleImg}
-        name="Google"
-        color="btn google"
-      />
-
-      </div>
-    </div>
+        <SocialSignupForm/>
   );
 }
 
---
+++ src/SocialSignupForm.js
@@ -0,0 +1,86 @@
+import React, { useState } from 'react';
+import './App.css'; 
+
+const SocialSignupForm = () => {
+  const [showForm, setShowForm] = useState(false);
+  const [selectedButton, setSelectedButton] = useState('');
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    alert(`Name: ${name}\nPassword: ${password}`);
+  };
+
+  return (
+    <div className="main-block">
+      <div className="block-item left">
+        <h1>Sign up with:</h1>
+        <p>
+          <small>
+            W3docs provides free learning materials for programming languages like HTML, CSS, JavaScript, PHP, etc.
+         </small>
+        </p>
+      </div>
+      <div className="block-item right">
+        <button
+          className="btn facebook"
+          onClick={() => {
+            setSelectedButton('Facebook');
+            setShowForm(true);
+          }}
+        >
+          <i className="fab fa-facebook-f"></i>
+          <span>Facebook</span>
+        </button>
+        <button
+          className="btn twitter"
+          onClick={() => {
+            setSelectedButton('Twitter');
+            setShowForm(true);
+          }}
+        >
+          <i className="fab fa-twitter"></i>
+          <span>Twitter</span>
+        </button>
+        <button
+          className="btn google"
+          onClick={() => {
+            setSelectedButton('Google');
+            setShowForm(true);
+          }}
+        >
+          <i className="fab fa-google"></i>
+          <span>Google</span>
+        </button>
+      </div>
+
+      {showForm && (
+        <div className="form-container">
+          <form onSubmit={handleFormSubmit}>
+            <h2>Signup with {selectedButton}</h2>
+            <div>
+              <input
+                type="text"
+                placeholder="Name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </div>
+            <div>
+              <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default SocialSignupForm;
